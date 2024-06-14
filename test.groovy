fa0e42950597d26e4b1343493bf1f560f2cc3617
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-06-14 18:36:01 +0530

    dd

--- test.groovy
+++ test.groovy
@@ -14,9 +14,9 @@ pipeline {
         stage('Test') {
             steps {
                 sh '''mvn sonar:sonar \\
-  -Dsonar.projectKey=studentapp \\
-  -Dsonar.host.url=http://52.91.116.200:9000 \\
-  -Dsonar.login=108f1ac163e9f9d6ba1f4c3d2269f84981411431'''
+                    -Dsonar.projectKey=studentapp \\
+                    -Dsonar.host.url=http://52.91.116.200:9000 \\
+                    -Dsonar.login=108f1ac163e9f9d6ba1f4c3d2269f84981411431'''
             }
         }
         stage('Deploy') {
